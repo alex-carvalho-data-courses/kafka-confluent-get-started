a283f16ace4174cac4c559561c831a724ff7820e
alex carvalho <alex.carvaho.data@gmail.com>
2022-10-10 01:00:44 +0200

    feature: kafka python producer

---
+++ kafka_confluent_get_started/producer.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser, FileType
+from configparser import ConfigParser
+from confluent_kafka import Producer
+from random import choice
+
+
+if __name__ == '__main__':
+    # Parse the command line.
+    parser = ArgumentParser()
+    parser.add_argument('config_file', type=FileType('r'))
+    args = parser.parse_args()
+
+    # Parse the configuration
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    config_parser = ConfigParser()
+    config_parser.read_file(args.config_file)
+    config = dict(config_parser['default'])
+
+    # Create producer instance
+    producer = Producer(config)
+
+    # Optional per-message delivery callback (triggered by poll() or flush())
+    # when a message has been successfully delivered or permanently failed
+    # delivery (after retries)
+    def delivery_callback(err, msg):
+        if err:
+            print(f'ERROR: Message failed delivery: {err}')
+        else:
+            print(f'Produced event to topic {msg.topic()}: '
+                  f'key = {msg.key().decode("utf-8")}, value = {msg.value().decode("utf-8")}')
+
+    # Produce data by selecting random values from these lists.
+    topic = 'purchases'
+    user_ids = ['eabara', 'jsmith', 'sgarcia', 'jbernard', 'htanaka', 'awalther']
+    products = ['book', 'alarm clock', 't-shirts', 'gift card', 'batteries']
+
+    count = 0
+    for _ in range(10):
+        user_id = choice(user_ids)
+        product = choice(products)
+        producer.produce(topic, product, user_id, callback=delivery_callback)
+
+    # Block until the messages are sent
+    producer.poll(10000)
+    producer.flush()
